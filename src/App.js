e719dd9ca59f709306a078220102ca4c215f647e
Fabrizio Platero <fabriplatero88@gmail.com>
2023-11-09 12:44:24 -0300

    added inter font

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 
 function App() {
   return (
-    <div className="font-[Poppins] bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] h-screen">
+    <div className="bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] h-screen">
       <NavBar />
       <ItemListContainer
         greeting={"Bievenido a RE-GAMES nuestra tienda de videojuegos"}
